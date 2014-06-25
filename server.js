140b2b8356ba8b916ec4dddff5473835099d5c3d
Eirik Reksten <eireksten@gmail.com>
2014-06-25 10:25:52 +0200

    Added express.js

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port);
+console.log('Server started! Listening on port ' + port + '.');
