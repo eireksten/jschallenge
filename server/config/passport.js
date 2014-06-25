40833065499b58259468b2fd2d53b5141d65249d
Eirik Reksten <eireksten@gmail.com>
2014-06-25 14:04:24 +0200

    Created basic login

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,19 +1,54 @@
 "use strict";
 
 var LocalStrategy   = require('passport-local').Strategy;
+var User = require('../model/user.js');
 
 module.exports = function(passport) {
 
   passport.serializeUser(function(user, done) {
-    done(null, user);
+    done(null, user.id);
   });
 
-  passport.deserializeUser(function(user, done) {
-    done(null, user);
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
   });
 
   passport.use(new LocalStrategy(function(username, password, done) {
-    return done(null, {username: username});
+
+    User.findOne({username: username}, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+
+      if (user) {
+        // Validate user
+        user.isValidPassword(password, function (err, valid) {
+          if (err)return done(err);
+          if (valid) {
+            return done(null, user);
+          }
+          return done(null, false, "Invalid Password");
+        });
+      }
+
+      // Register user
+
+      var newuser = new User();
+
+      newuser.username = username;
+      newuser.generateHash(password, function (err, hash) {
+        if (err)return done(err);
+
+        newuser.password = hash;
+        newuser.save(function (err) {
+          if (err)throw err;
+          return done(null, newuser);
+        });
+
+      });
+    });
   }));
 
 };
--- server/model/user.js
+++ server/model/user.js
@@ -1,8 +1,20 @@
+"use strict";
+
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
   username: String,
   password: String
 });
+
+userSchema.methods.generateHash = function (password, next) {
+  bcrypt.hash(password, bcrypt.genSaltSync(8), null, next);
+};
+
+userSchema.methods.isValidPassword = function (password, next) {
+  bcrypt.compare(password, this.password, next);
+};
+
 
 module.exports = mongoose.model('User', userSchema);
