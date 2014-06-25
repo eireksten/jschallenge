d4d2dd2c18b2e743314f8ecbe01972453c2f5424
Eirik Reksten <eireksten@gmail.com>
2014-06-25 11:13:03 +0200

    Extracted routes into own module

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var path = require('path');
+
+var routes = require('./server/routes.js');
 
 var app = express();
 var port = process.env.PORT || 1337;
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+
+app.use(express.static(path.join(__dirname, 'client'))); 
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+routes(app);
+
 
 app.listen(port);
 console.log('Server started! Listening on port ' + port + '.');
